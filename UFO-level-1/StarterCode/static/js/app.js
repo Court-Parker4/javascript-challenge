237862829eaecb6d3c38d47c5d8e94f093253d3a
Court-Parker4 <courtlm4@gmail.com>
2021-03-06 09:29:18 -0800

    fix filter date in html file as data provided is only for 2010

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -2,27 +2,11 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-// console.log(tableData);
 var tbody=d3.select('#ufo-table').select('tbody');
 
 for (var i=0; i<tableData.length; i++){
     var curr_row=tableData[i];
     var row=tbody.append('tr')
-    //Alternative method
-    // var value=row.append('td')
-    // value.text(curr_row['datetime']);
-    // value=row.append('td')
-    // value.text(curr_row['city']);
-    // value=row.append('td')
-    // value.text(curr_row['state']);
-    // value=row.append('td')
-    // value.text(curr_row['country']);
-    // value=row.append('td')
-    // value.text(curr_row['shape']);
-    // value=row.append('td')
-    // value.text(curr_row['durationMinutes']);
-    // value=row.append('td')
-    // value.text(curr_row['comments']);
     Object.keys(curr_row).forEach(each_key=>{
         var value=row.append('td');
         value.text(curr_row[each_key]);
@@ -33,11 +17,6 @@ function handleClick(){
     tbody.html('');
     var filter_input=d3.select('#datetime').property('value');
     console.log(filter_input)
-    //Alternative method
-    // console.log(filter_input.property('value'));
-    // var filter_table=tableData.filter(element => function plot(element) {
-    //     element['datetime']==filter_input;
-    // });
     var filter_table=tableData.filter(element => element['datetime']==filter_input);
     console.log(filter_table);
     for (var i=0; i<filter_table.length; i++){
